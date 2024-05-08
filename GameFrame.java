bb7b9fece9d5fed34e44a8a3caed91b728d38e2c
Carlos Eduardo Santos <134026157+HyCadu@users.noreply.github.com>
2024-05-08 11:19:53 -0300

    Update GameFrame.java
    Remoção do botão de reiniciar a bola (única função do botão era criar uma nova bola para corrigir o erro dela travada no centro da tela, o erro foi corrigido)

--- GameFrame.java
+++ GameFrame.java
@@ -9,18 +9,9 @@ import javax.swing.*;
 public class GameFrame extends JFrame{
     
     GamePanel painel = new GamePanel();
-    JButton resetButton = new  JButton("Reiniciar bola");
     
     GameFrame(){
      this.add(painel);
-    this.add(resetButton, BorderLayout.SOUTH);
-        resetButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                painel.newBall(); // As vezes a bola ficava presa quicando apenas no meio, tentei fazer um botão para reiniciar ela.
-                painel.requestFocusInWindow();
-            }
-        });
-
      this.setTitle("JOGO PONG");
      this.setResizable(false);
      this.setBackground(Color.black);
